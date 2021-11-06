d2e99e7d7140af8141e250bb07d410f0d9464f36
BLUDUDE02 <stacyscouting@hotmail.com>
2021-11-05 23:11:59 -0400

    Level 1 Mechanics

--- GameFiles/Bug Game/Assets/Scripts/MoveSpire.cs
+++ GameFiles/Bug Game/Assets/Scripts/MoveSpire.cs
@@ -6,26 +6,31 @@ public class MoveSpire : MonoBehaviour
 {
     public ButtonControl button1, button2, button3, button4;
     public bool b1 = false, b2 = false, b3 = false, b4 = false;
+    public Animator anim;
 
     private void Update()
     {
         if(button1.isButtonClicked && !b1)
         {
+            anim.SetTrigger("IsMoving");
             StartCoroutine(MoveDown());
             b1 = true;
         }
         if (button2.isButtonClicked && !b2)
         {
+            anim.SetTrigger("IsMoving");
             StartCoroutine(MoveDown());
             b2 = true;
         }
         if (button3.isButtonClicked && !b3)
         {
+            anim.SetTrigger("IsMoving");
             StartCoroutine(MoveDown());
             b3 = true;
         }
         if (button4.isButtonClicked && !b4)
         {
+            anim.SetTrigger("IsMoving");
             StartCoroutine(MoveDown());
             b4 = true;
         }
@@ -38,6 +43,7 @@ public class MoveSpire : MonoBehaviour
         {
             if (transform.position == newPos)
             {
+                anim.ResetTrigger("IsMoving");
                 yield break;
             }
             transform.position = Vector3.MoveTowards(transform.position, newPos, 0.1f);
